ad66f0e1fd6df123da78ae12c6385626c0ac9afd
coufxr <mu993364@163.com>
2023-12-03 20:08:52 +0800

    feat: __init__

---
+++ src/constants.rs
@@ -0,0 +1,5 @@
+pub(crate) const WIDTH: usize = 30;
+pub(crate) const HEIGHT: usize = 20;
+pub(crate) const SPEED: usize = 800;
+pub(crate) const SNAKE_HEAD: &str = "●";
+pub(crate) const SNAKE_BODY: &str = "◈";
